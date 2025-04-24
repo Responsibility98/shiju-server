a8a3dab5c70590f615f947a966584cdaf0cda81d
yan.zhang <yan.zhang@compassmobi.com>
2025-04-24 12:19:46 +0800

    init

---
+++ src/main/java/org/example/Application.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        // 启动 Spring Boot 应用
+        SpringApplication.run(Application.class, args);
+    }
+}
